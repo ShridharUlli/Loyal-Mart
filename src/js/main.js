c2676b77ca481e091597484714b02b675f79c074
Shridhar Ulli <shridharulli@gmail.com>
2021-05-19 10:20:15 +0530

    changes

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,13 @@ var swiper = new Swiper(".mySwiper", {
       el: ".swiper-pagination",
     },
   });
+
+  var swiper = new Swiper(".trending", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
