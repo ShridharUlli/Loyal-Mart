f4333c39446c4163ca1eb40ad98e57825ca61b1b
Shridhar Ulli <shridharulli@gmail.com>
2021-05-30 20:44:35 +0530

    changes

--- src/js/main.js
+++ src/js/main.js
@@ -13,3 +13,33 @@ var swiper = new Swiper(".mySwiper", {
       clickable: true,
     },
   });
+
+
+
+  //menu opener
+
+  let menuIcon = document.querySelector("#menu");
+  let menuContent = document.querySelector("#menuContent");
+  let hamburger = document.querySelector("#menu > #hamburger");
+  let close = document.querySelector("#menu > #close");
+  menuIcon.addEventListener("click",function(e){
+    e.preventDefault();
+    if (hamburger.classList.contains("block") && close.classList.contains("hidden")) {
+      hamburger.classList.remove("block");
+      hamburger.classList.add("hidden");
+      close.classList.remove("hidden");
+      close.classList.add("block");    
+      menuContent.classList.remove("hidden");             
+      console.log("testing1");
+
+      } else{
+        hamburger.classList.add("block");
+        hamburger.classList.remove("hidden");
+        close.classList.add("hidden");
+        close.classList.remove("block");    
+        menuContent.classList.add("hidden"); 
+        console.log("testing2");
+      } 
+  });
+
+
