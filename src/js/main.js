b35fbeec2b89305e025d6238b530d90a2dfcdd43
Shridhar Ulli <shridharulli@gmail.com>
2021-06-08 19:10:57 +0530

    changes

--- src/js/main.js
+++ src/js/main.js
@@ -47,3 +47,92 @@ var swiper = new Swiper(".mySwiper", {
   });
 
 
+  function RadioFields() {
+    return {
+        value: false, 
+        init() {
+          this.value = this.$el.querySelector('input[type=radio]:checked').value
+        },      
+    }
+}
+window.RadioFields = RadioFields;
+
+//checkbox
+
+
+// let numberButton  = document.querySelector('input[name="number"]');
+
+//   numberButton.addEventListener("change", function(){
+//     console.log(numberButton.checked)
+    
+//   })
+// var radios = document.querySelectorAll('input[type=radio][name="number"]');
+
+// function changeHandler(event) {
+//    if ( this.checked) {
+//      let classList = this.nextElementSibling.classList;
+//      classList.remove("text-teal-600");
+//      classList.add("bg-teal-600", "text-white");
+//      console.log(this);
+//    } else if ( !(this.checked) ) {
+//       classList.remove("text-teal-600");
+//       classList.add("bg-teal-600", "text-white");
+//    }  
+// }
+
+// Array.prototype.forEach.call(radios, function(radio) {
+//    radio.addEventListener('change', changeHandler);
+// });
+
+// if(phoneNumbers.length){
+//   let phoneNumbersClassList = phoneNumbers.nextElementSibling.classList;
+//   phoneNumbers.addEventListener("change",function(){
+//     phoneNumbersClassList.remove("text-teal-600");
+//     phoneNumbersClassList.add("bg-teal-600","text-white");
+//   })
+// }else if(phoneNumberSelected.length){
+//   let phoneNumbersSelectedClassList = phoneNumberSelected.nextElementSibling.classList;
+//   phoneNumberSelected.addEventListener("change",function(){
+//     phoneNumbersSelectedClassList.add("text-teal-600");
+//     phoneNumbersSelectedClassList.remove("bg-teal-600","text-white");
+//   })
+// }
+
+
+// let phoneNumbers  = document.querySelectorAll('input[name="number"]');
+// let phoneNumberSelected = document.querySelectorAll('input[name="number"]:checked');
+
+// if(phoneNumbers.length){
+//   let phoneNumbersClassList = phoneNumbers.nextElementSibling.classList;
+//   phoneNumbers.addEventListener("change",function(){
+//     phoneNumbersClassList.remove("text-teal-600");
+//     phoneNumbersClassList.add("bg-teal-600","text-white");
+//   })
+// }else if(phoneNumberSelected.length){
+//   let phoneNumbersSelectedClassList = phoneNumberSelected.nextElementSibling.classList;
+//   phoneNumberSelected.addEventListener("change",function(){
+//     phoneNumbersSelectedClassList.add("text-teal-600");
+//     phoneNumbersSelectedClassList.remove("bg-teal-600","text-white");
+//   })
+// }
+
+
+
+
+
+// let phoneNumberSelected = document.querySelector('input[name="number"]:checked');
+
+// let phoneNumberSelected = document.querySelectorAll('input[name="number"]');
+// phoneNumberSelected.forEach(element => {
+//   element.addEventListener("change", (e) => {
+//     let classList = phoneNumberSelected.nextElementSibling.classList();
+//     if(e.checked){
+//       classList.remove("text-teal-600");
+//       classList.add("bg-teal-600","text-white");
+//     }else{
+//       classList.add("text-teal-600");
+//       classList.remove("bg-teal-600","text-white");
+//     }
+//   })
+  
+// });
